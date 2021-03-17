eb147771d78d121d8762f20d2caa6269199e1a83
arndzk <22920826+arndzk@users.noreply.github.com>
2021-03-17 21:10:31 +0200

    meals are now rendered in their respective categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>itemData.item.title</Text>
+      </View>
+    );
+  };
+
   const catId = props.navigation.getParam('categoryId');
+  const displayMeals = MEALS.filter((meal) => {
+    meal.categoryIds.indexOf(catId) >= 0;
+  });
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen!</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title="Go to Meal Details"
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: 'MealDetails',
-          });
-        }}
+      <FlatList
+        data={displayMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
       />
     </View>
   );
